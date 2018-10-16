ac3772a573a72329787cfd2ee8072a87fcaee28f
Pongchanin Sakulrattanasak <ps527@sci.tu.ac.th>
2018-10-16 14:16:05 +0700

    new

---
+++ platformPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="platformPack_tilesheet" tilewidth="24" tileheight="32" tilecount="518" columns="37">
+ <image source="Project 2/kenney_simplifiedplatformer/Tilesheet/platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
